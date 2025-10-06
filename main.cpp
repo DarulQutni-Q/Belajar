9debd4dcd66146f9271af798fc2fc04d7ec147e5
Darul Qutni <darul.250180122@mhs.unimal.ac.id>
2025-10-06 12:40:31 +0700

    belajar

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+// C++ created by Bjarne Stroustrup
+// #include <iostream>
+//
+// int main(int argc, char *argv[]) {
+//   std::cout << "Hello World";
+//   return 0;
+// }
+//
+// In C++, the std::cout statement, short for character output,
+// tells the terminal to output a message. It’s followed by <<, two left arrows.
+// The message we want to display should be surrounded by " double quotes. And
+// // every C++ statement ends with a ; semicolon.
 #include <iostream>
-using namespace std;
-
 int main() {
-  cout << "Nama saya darul";
-  cout << "halo";
+  std::cout << "🔥 AC/DC - Highway to Hell\n";
+  std::cout << "🌈 Pink Floyd - Comfortably Numb\n";
+  std::cout << "🚓 The Police - Message in a Bottle\n";
+  std::cout << "🤘 The Clash - London Calling\n";
+  std::cout << "💔 Pat Benatar - Heartbreaker\n";
 }
